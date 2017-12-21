2d0fdfd0a5ceec823462ae13d09dca7075d66aba
Omar Tawfik <Xbox.ps3.psp.ps2@gmail.com>
2017-12-20 22:52:20 -0800

    Update symbols.m

--- v0rtex-S/symbols.m
+++ v0rtex-S/symbols.m
@@ -102,7 +102,7 @@ BOOL init_symbols()
             OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070a5c40;
             OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006eee1b8;
             OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064b5178;
-            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074961a8;
+            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074335d0;
             OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075b40b0;
         }
     }
@@ -126,8 +126,32 @@ BOOL init_symbols()
             OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff007099a38;
             OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006f25538;
             OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff006526178;
-            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff00748a4b4;
+            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074278dc;
             OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075a80b0;
+        }
+    }
+    
+    else if (strcmp(u.machine, "iPhone8,2") == 0)
+    {
+        if ([ver  isEqual: @"Version 10.3.3 (Build 14G60)"])
+        {
+            OFFSET_ZONE_MAP                             = 0xfffffff007558478;
+            OFFSET_KERNEL_MAP                           = 0xfffffff0075b4050;
+            OFFSET_KERNEL_TASK                          = 0xfffffff0075b4048;
+            OFFSET_REALHOST                             = 0xfffffff00753aba0;
+            OFFSET_BZERO                                = 0xfffffff00708df80;
+            OFFSET_BCOPY                                = 0xfffffff00708ddc0;
+            OFFSET_COPYIN                               = 0xfffffff00718d028;
+            OFFSET_COPYOUT                              = 0xfffffff00718d21c;
+            OFFSET_CHGPROCCNT                           = 0xfffffff007055dff;
+            OFFSET_KAUTH_CRED_REF                       = 0xfffffff007374d90;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff0070a60b4;
+            OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070b938c;
+            OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070a5bd8;
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006eee1b8;
+            OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064b2178;
+            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff007433ab4;
+            OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075b40b0;
         }
     }
     
