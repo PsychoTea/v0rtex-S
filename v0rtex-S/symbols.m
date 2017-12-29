4c973f9c5d5d79efc5d1b6c63e8e0ceac892daaf
xNako <nakocdm8@gmail.com>
2017-12-29 11:11:20 +0100

    add ipad mini 3 offsets
    iPad 4,7 offsets for 10.3.x iOS versions
    
    Signed-off-by: xNako <nakocdm8@gmail.com>

--- v0rtex-S/symbols.m
+++ v0rtex-S/symbols.m
@@ -186,6 +186,102 @@ BOOL init_symbols()
         }
     }
     
+    // iPad Mini 3 (Wifi)
+    if (strcmp(u.machine, "iPad4,7") == 0)
+    {
+        if ([ver isEqual: @"Version 10.3.3 (Build 14G60)"])
+        {
+            OFFSET_ZONE_MAP                        0xfffffff00754c478
+            OFFSET_KERNEL_MAP                      0xfffffff0075a8050
+            OFFSET_KERNEL_TASK                     0xfffffff0075a8048
+            OFFSET_REALHOST                        0xfffffff00752eba0
+            OFFSET_BZERO                           0xfffffff007081f80
+            OFFSET_BCOPY                           0xfffffff007081dc0
+            OFFSET_COPYIN                          0xfffffff007180e98
+            OFFSET_COPYOUT                         0xfffffff00718108c
+            OFFSET_CHGPROCCNT                      0xfffffff00738e77c
+            OFFSET_KAUTH_CRED_REF                  0xfffffff007368b08
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          0xfffffff007099f14
+            OFFSET_IPC_KOBJECT_SET                 0xfffffff0070ad1ec
+            OFFSET_IPC_PORT_MAKE_SEND              0xfffffff007099a38
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    0xfffffff006f2d738
+            OFFSET_OSSERIALIZER_SERIALIZE          0xfffffff007441908
+            OFFSET_ROP_LDR_X0_X0_0x10              0xfffffff006408a84
+            OFFSET_ROP_ADD_X0_X0_0x10              0xfffffff0064ba174
+            OFFSET_ROOT_MOUNT_V_NODE               0xfffffff0075a80b0
+            return TRUE;
+        }
+        
+        if ([ver isEqual: @"Version 10.3.2 (Build 14F89)"])
+        {
+            OFFSET_ZONE_MAP                        0xfffffff00754c478
+            OFFSET_KERNEL_MAP                      0xfffffff0075a8050
+            OFFSET_KERNEL_TASK                     0xfffffff0075a8048
+            OFFSET_REALHOST                        0xfffffff00752eba0
+            OFFSET_BZERO                           0xfffffff007081f80
+            OFFSET_BCOPY                           0xfffffff007081dc0
+            OFFSET_COPYIN                          0xfffffff0071811ec
+            OFFSET_COPYOUT                         0xfffffff0071813e0
+            OFFSET_CHGPROCCNT                      0xfffffff00738e82c
+            OFFSET_KAUTH_CRED_REF                  0xfffffff007368be4
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          0xfffffff007099f14
+            OFFSET_IPC_KOBJECT_SET                 0xfffffff0070ad1ec
+            OFFSET_IPC_PORT_MAKE_SEND              0xfffffff007099a38
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    0xfffffff006f2d738
+            OFFSET_OSSERIALIZER_SERIALIZE          0xfffffff0074419b8
+            OFFSET_ROP_LDR_X0_X0_0x10              0xfffffff00640ca84
+            OFFSET_ROP_ADD_X0_X0_0x10              0xfffffff0064be174
+            OFFSET_ROOT_MOUNT_V_NODE               0xfffffff0075a80b0
+            return TRUE;
+        }
+        
+        if ([ver isEqual: @"Version 10.3.1 (Build 14E304)"])
+        {
+            OFFSET_ZONE_MAP                        0xfffffff00754c478
+            OFFSET_KERNEL_MAP                      0xfffffff0075a8050
+            OFFSET_KERNEL_TASK                     0xfffffff0075a8048
+            OFFSET_REALHOST                        0xfffffff00752eba0
+            OFFSET_BZERO                           0xfffffff007081f80
+            OFFSET_BCOPY                           0xfffffff007081dc0
+            OFFSET_COPYIN                          0xfffffff007181218
+            OFFSET_COPYOUT                         0xfffffff00718140c
+            OFFSET_CHGPROCCNT                      0xfffffff00738e504
+            OFFSET_KAUTH_CRED_REF                  0xfffffff0073688a4
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          0xfffffff007099f7c
+            OFFSET_IPC_KOBJECT_SET                 0xfffffff0070ad1d4
+            OFFSET_IPC_PORT_MAKE_SEND              0xfffffff007099aa0
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    0xfffffff006f2d738
+            OFFSET_OSSERIALIZER_SERIALIZE          0xfffffff007441424
+            OFFSET_ROP_LDR_X0_X0_0x10              0xfffffff00640ca84
+            OFFSET_ROP_ADD_X0_X0_0x10              0xfffffff0064bd174
+            OFFSET_ROOT_MOUNT_V_NODE               0xfffffff0075a80b0
+            return TRUE;
+        }
+        
+        if ([ver isEqual: @"Version 10.3 (Build 14E277)"])
+        {
+            OFFSET_ZONE_MAP                        0xfffffff00754c478
+            OFFSET_KERNEL_MAP                      0xfffffff0075a8050
+            OFFSET_KERNEL_TASK                     0xfffffff0075a8048
+            OFFSET_REALHOST                        0xfffffff00752eba0
+            OFFSET_BZERO                           0xfffffff007081f80
+            OFFSET_BCOPY                           0xfffffff007081dc0
+            OFFSET_COPYIN                          0xfffffff007181218
+            OFFSET_COPYOUT                         0xfffffff00718140c
+            OFFSET_CHGPROCCNT                      0xfffffff00738e504
+            OFFSET_KAUTH_CRED_REF                  0xfffffff0073688a4
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          0xfffffff007099f7c
+            OFFSET_IPC_KOBJECT_SET                 0xfffffff0070ad1d4
+            OFFSET_IPC_PORT_MAKE_SEND              0xfffffff007099aa0
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    0xfffffff006f2d738
+            OFFSET_OSSERIALIZER_SERIALIZE          0xfffffff007441424
+            OFFSET_ROP_LDR_X0_X0_0x10              0xfffffff00640ca84
+            OFFSET_ROP_ADD_X0_X0_0x10              0xfffffff0064bd174
+            OFFSET_ROOT_MOUNT_V_NODE               0xfffffff0075a80b0
+            return TRUE;
+        }
+    }
+    
     LOG("Device not supported. \n");
     return FALSE;
 }
